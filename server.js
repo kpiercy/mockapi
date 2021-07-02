3d712e7ac5bae76043ee9ef225884d8907ac181f
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-07-02 14:30:10 -0400

    Revert "test"
    This reverts commit dfb3265883f785089066af6d65c85e7c6aa87a69.

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const rateLimit = require('express-rate-limit')
 const limiter = rateLimit({
     windowMs: 30 * 60 * 1000, //30 min
     max: 100, //limit each IP to 100 requests per 30min
+    message: "Too many requests from this IP in the last 30 min, please try again later."
 })
 
 //auth
