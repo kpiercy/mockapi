c448b86a90afc2d6d25d1259051e524af0e0289a
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-11-09 00:53:36 -0500

    +html email for authIP mw and logging

--- middleware/ipAccess.js
+++ middleware/ipAccess.js
@@ -1,9 +1,11 @@
 const sql = require('mssql/msnodesqlv8')
 const configJobData = require('../config/JobData_dbconfig')
 const nodemailer = require("nodemailer")
+const jwt = require('jsonwebtoken')
 
 async function authIP(req, res, next){
     try{
+        //setup handling for when req is from /refresh for the token assignment
         const authHeader = req.headers['authorization']
         const token = authHeader && authHeader.split(' ')[1]
         let pool = await sql.connect(configJobData)
@@ -18,32 +20,38 @@ async function authIP(req, res, next){
                 res.status(403).send('Access not allowed from this IP. Please check your email to perform additional verification.')
                 //perform 2fa here
                 async function sendemail() {
-    
                     // create reusable transporter object using the default SMTP transport
                     let transporter = nodemailer.createTransport({
-                        host: "smtp.socketlabs.com",
-                        port: 587,
-                        secure: false, // true for 465, false for other ports
-                        auth: {
-                            user: process.env.SMTP_USER,
-                            pass: process.env.SMTP_PASS,
-                        },
+                    host: "smtp.socketlabs.com",
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                        user: process.env.SMTP_USER,
+                        pass: process.env.SMTP_PASS,
+                    },
+                })
+            
+                // send mail with defined transport object
+                let info = await transporter.sendMail({
+                    from: '"ElitePS API" <api@eliteps.com>', // sender address
+                    to: `${thisUser}`, // list of receivers
+                    subject: "Please confirm identity to access ElitePS API", // Subject line
+                    text: "Hello, Additional verification is required to continue access to the ElitePS API. If you do not see a 'Confirm Me' button below, please reach out to kpiercy@eliteps.com for additional assistance.", // plain text body
+                    html: `<!doctype html>
+                    <html>
+                      <head>
+                        <meta charset="utf-8">
+                      </head>
+                      <body>
+                      <p>Hello,<br> Additional verification is required to continue access to the ElitePS API. Please click the link below to reinitiate access:<br><form action="http://localhost:4000/clients/users/me/confirm" method="post" enctype="multipart/form-data"><input type="hidden" name="headers[Authorization]" value="Bearer ${token}" /><input id='submitButton' type='submit' value='Confirm Me'/></form></p>
+                      </body>
+                    </html>`
                     })
-                
-                    // send mail with defined transport object
-                    let info = await transporter.sendMail({
-                        from: '"ElitePS API" <api@eliteps.com>', // sender address
-                        to: `${thisUser}`, // list of receivers
-                        subject: "Please confirm identity to access ElitePS API", // Subject line
-                        text: "Hello world?", // plain text body
-                        html: "<b>Hello world?</b>", // html body
-                        })
-                
-                    console.log("Message sent: %s", info.messageId)
-                    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-                
-                    }
-                    sendemail().catch(console.error)
+            
+                console.log("Verification email sent: %s", info.messageId)
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                }
+                sendemail().catch(console.error)
                 } 
              else {
                 next()
@@ -53,5 +61,6 @@ async function authIP(req, res, next){
     }
     
 }
+
 
 module.exports = authIP;
