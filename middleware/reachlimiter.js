3cb29137126f4429bbe0e6115ac238095d446922
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-11-29 10:56:29 -0500

    updated err handle and logging reachlimiter

--- middleware/reachlimiter.js
+++ middleware/reachlimiter.js
@@ -3,33 +3,47 @@ require('dotenv').config()
 const sql = require('mssql/msnodesqlv8')
 const configJobData = require('../config/JobData_dbconfig')
 
+//handles the requirement that every request this mw is passed on should contain clientid and determines if the requesting user is a part of the clientid in the user table
 
 async function limitReach(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    req.params.cid = req.cid //retrieve cid from originalUrl
-    const cid = req.params.cid
+    if ( req.params.clientid == null ){
+        req.params.clientid = req.clientid
+        var cid = req.clientid
+    } else {
+        var cid = req.params.clientid
+    }
 
-    if (token == null) return res.sendStatus(401)
+    let text2 = cid
+    var cid = text2.toUpperCase()
+
+
+    if (token == null) return res.status(401)
 
     try{
+        var master = false
         let pool = await sql.connect(configJobData)
         let limit = await pool.request()
             .input('token', sql.VarChar, token)
             .execute('GetClientReach')
         var thisReach = limit.recordset[0].clientid
-        var thisPerm = limit.recordset[0].permissions
-            
-            if (thisReach !== cid) {
-                res.status(401).json('Requesting user does not belong to the specified client contained in the url. You can use /clients/users/me to retrieve the correct client id.')
-            } else if (thisReach === process.env.EPS_CLIENT_ID) { 
-                let reqUser = 'ElitePS Admin'
-                console.log('ElitePS user reach verified')
+        var thisUser = limit.recordset[0].username
+        try {
+            if ( thisReach.toUpperCase() === process.env.EPS_CLIENT_ID ) { 
+                var master = true
+                console.log('ElitePS user: '+`${thisUser}`+' invoked master reach')
                 next()
+            } else if ( thisReach !== cid && master === false ) {
+                    res.status(401).json('Requesting user does not belong to the specified client contained in "clientid". You can use /clients/users/me to retrieve the correct client id.')
             } else {
                 console.log('User reach verified')
                 next()
             }
+        } catch {
+            res.status(401).json('Error: unable to call next()')
+        }    
+
     } catch {
         res.status(500).json('Unable to verify user reach by client id specified')
     }
