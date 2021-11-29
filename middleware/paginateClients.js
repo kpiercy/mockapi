f5dd23dce70f0e75c7b4164de159d7525e7b79af
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-11-29 10:52:13 -0500

    modified for controllers

--- middleware/paginateClients.js
+++ middleware/paginateClients.js
@@ -1,39 +1,48 @@
 require('dotenv').config()
 const sql = require('mssql/msnodesqlv8')
-const dboperations = require('../services/dbops_clients')
+const dboperations = require('../controllers/dbops_clients')
 const configJobData = require('../config/JobData_dbconfig')
 const configEliteMaster = require('../config/EliteMaster_dbconfig')
 const model = require('../models/client')
 
 function paginatedResults(model) {
     return async (req, res, next) => {
+
+        //req.params.clientid = req.clientid
+        var cid = req.params.clientid
+
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
         const startIndex = (page -1) * limit
         const endIndex = page * limit
     
+        if( cid == null ) {
+            res.status(406).json('Error: clientid must be specified in either the URL as a query param or in the request body.')
+        } else {
         const results = {}
     
         if( endIndex < model.length ) {
             var nextPage = page + 1
-        results.next = "http://localhost:3000/clients?page="+nextPage+"&limit="+limit+""
+        results.next = "http://localhost:3000/clients/"+`${cid}`+"?page="+nextPage+"&limit="+limit+""
             
         }
         if( startIndex > 0) {
             var prevPage = page - 1
-        results.previous = "http://localhost:3000/clients?page="+prevPage+"&limit="+limit+""
+        results.previous = "http://localhost:3000/clients/"+`${cid}`+"?page="+prevPage+"&limit="+limit+""
         }
         try{
             let pool = await sql.connect(configJobData)
             results.results = await pool.request()
                 .input('startindex', sql.Int, startIndex)
                 .input('limit', sql.Int, limit)
+                .input('cid', sql.VarChar, cid)  //only retrieve proofs associated to this req users clientid
                 .execute('GetPaginatedClients')
             res.paginatedResults = results
             next()
         } catch (e) {
             res.status(500).json( {message: e.message} )
         }
+    }
     }
 }
 
--- middleware/paginateProofs.js
+++ middleware/paginateProofs.js
@@ -1,42 +1,50 @@
 require('dotenv').config()
 const sql = require('mssql/msnodesqlv8')
-const dboperations = require('../services/dbops_proofs')
+const dboperations = require('../controllers/dbops_proofs')
 const configJobData = require('../config/JobData_dbconfig')
 const configEliteMaster = require('../config/EliteMaster_dbconfig')
 const model = require('../models/proof')
 
 function paginatedResults(model) {
     return async (req, res, next) => {
-        req.params.cid = req.cid //retrieve cid from originalUrl
-        var cid = req.params.cid
+        req.params.clientid = req.clientid
+        var cid = req.params.clientid
+
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
         const startIndex = (page -1) * limit
         const endIndex = page * limit
-    
-        const results = {}
+
+        if( cid == null ) {
+            res.status(406).json('Error: clientid must be specified in either the URL as a query param or in the request body.')
+        } else {
+            const results = {}
     
         if( endIndex < model.length ) {
             var nextPage = page + 1
-        results.next = "http://localhost:3000/clients/"+`${cid}`+"/jobs/files/proofs?page="+nextPage+"&limit="+limit+""
+        results.next = "http://localhost:3000/clients/jobs/files/proofs?page="+nextPage+"&limit="+limit+""
             
         }
         if( startIndex > 0) {
             var prevPage = page - 1
-        results.previous = "http://localhost:3000/clients/"+`${cid}`+"/jobs/files/proofs?page="+prevPage+"&limit="+limit+""
+        results.previous = "http://localhost:3000/clients/jobs/files/proofs?page="+prevPage+"&limit="+limit+""
         }
         try{
             let pool = await sql.connect(configJobData)
-            results.results = await pool.request()
+            results.data = await pool.request()
                 .input('startindex', sql.Int, startIndex)
                 .input('limit', sql.Int, limit)
-                //.input('client', sql.VarChar, cid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this req users clientid
+                .input('cid', sql.VarChar, cid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this req users clientid
+                //.input('job', sql.UniqueIdentifier, jid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this jobid
+                //.input('file', sql.UniqueIdentifier, fid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this fileid
                 .execute('GetPaginatedProofs')
             res.paginatedResults = results
             next()
         } catch (e) {
             res.status(500).json( {message: e.message} )
         }
+        }
+
     }
 }
 
