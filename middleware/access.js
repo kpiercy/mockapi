89992d6dc9789bba0edbc5c400a0e6fe024fcd47
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-11-29 10:58:14 -0500

    add addtl layer of access check for client lvl

--- middleware/access.js
+++ middleware/access.js
@@ -14,11 +14,17 @@ async function apiAccess(req, res, next) {
         let canAccess = await pool.request()
             .input('token', sql.VarChar, token)
             .execute('CanAccessAPI')
-        var thisUserAccess = canAccess.recordset[0].apiaccess
-            if (thisUserAccess !== 'true') {
+            var thisUserAccess = canAccess.recordset[0].apiaccess
+            var thisUserClient = canAccess.recordset[0].clientid
+        let pool2 = await sql.connect(configJobData)
+        let clientAccess = await pool.request()
+            .input('clientid', sql.VarChar, thisUserClient)
+            .execute('ClientAccessAPI')
+            var thisClientAccess = clientAccess.recordset[0].status
+        if (thisUserAccess === 'true' && thisClientAccess === 'Active') {
+            next()
+        } else {
                 res.status(401).json('Requesting user does not have API access. Please contact ElitePS for more information.')
-            } else {
-                next()
             }
     } catch {
         res.status(500).json('Unable to retrieve apiAccess for user.')
