8c016fd7a02bfd940dfdb13d44c841305274ed6b
kpiercy <67161128+kpiercy@users.noreply.github.com>
2021-12-15 10:29:43 -0500

    add enviro control for dotenv to routes

--- routes/versfiles.js
+++ routes/versfiles.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if ( process.env.ENVIRONMENT !== 'production' ) {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const router = express.Router()
 
