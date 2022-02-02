fdd6b3cfeaf48d04f1d3cbf21f5c5a92dad8fb09
Kraig Piercy <kapiercy11@gmail.com>
2022-02-02 00:14:42 -0500

    add paginate control param for get clients

--- controllers/dbops_clients.js
+++ controllers/dbops_clients.js
@@ -22,39 +22,53 @@ const clients_client_all = async (req, res) => {
     req.clientid = req.params.clientid
     req.params.clientid = req.clientid
     var cid = req.params.clientid
+    var pageIt = req.query.paginate.toLowerCase()
+    
+    if ( pageIt === 'true' ) {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
 
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
-
-    if (cid == null) {
+        if (cid == null) {
         res.status(406).json('Error: clientid must be specified in either the URL as a query param or in the request body.')
-    } else {
+        } else {
         const results = {}
 
-        if (endIndex < model.length) {
-            var nextPage = page + 1
-            results.next = "http://localhost:3000/clients?page=" + nextPage + "&limit=" + limit + ""
+            if (endIndex < model.length) {
+                var nextPage = page + 1
+                results.next = "http://localhost:3000/clients?page=" + nextPage + "&limit=" + limit + ""
 
+            }
+            if (startIndex > 0) {
+                var prevPage = page - 1
+                results.previous = "http://localhost:3000/clients?page=" + prevPage + "&limit=" + limit + ""
+            }
+            try {
+                let pool = await sql.connect(configJobData)
+                results.data = await pool.request()
+                    .input('startindex', sql.Int, startIndex)
+                    .input('limit', sql.Int, limit)
+                    .input('cid', sql.VarChar, cid)
+                    //.input('job', sql.UniqueIdentifier, jid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this jobid
+                    //.input('file', sql.UniqueIdentifier, fid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this fileid
+                    .execute('GetPaginatedClients')
+                res.paginatedResults = results
+                res.status(200).json(res.paginatedResults)
+            } catch (e) {
+                res.status(500).json({ message: e.message })
         }
-        if (startIndex > 0) {
-            var prevPage = page - 1
-            results.previous = "http://localhost:3000/clients?page=" + prevPage + "&limit=" + limit + ""
-        }
+    }
+    } else {
         try {
-            let pool = await sql.connect(configJobData)
-            results.data = await pool.request()
-                .input('startindex', sql.Int, startIndex)
-                .input('limit', sql.Int, limit)
-                .input('cid', sql.VarChar, cid)
-                //.input('job', sql.UniqueIdentifier, jid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this jobid
-                //.input('file', sql.UniqueIdentifier, fid)  //CHANGE GetPaginatedProofs to only retrieve proofs associated to this fileid
-                .execute('GetPaginatedClients')
-            res.paginatedResults = results
-            res.status(200).json(res.paginatedResults)
-        } catch (e) {
-            res.status(500).json({ message: e.message })
+                let pool = await sql.connect(configJobData)
+                let client = await pool.request()
+                    .input('cid', sql.VarChar, cid)
+                    .execute('GetClient')
+                
+                res.json(JSON.parse(client.recordset[0]['JSON_F52E2B61-18A1-11d1-B105-00805F49916B']))
+            } catch (e) {
+                res.status(500).json({ message: e.message })
         }
     }
 }
