e8e8aa738d516303630596f35c02859a298db79f
Kraig Piercy <kapiercy11@gmail.com>
2022-02-15 16:40:28 -0500

    remove config files from gitignore

---
+++ config/JobData_dbconfig.js
@@ -0,0 +1,22 @@
+const sql = require('mssql/msnodesqlv8');
+require('dotenv').config()
+
+const configJobData = {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    server: process.env.SQL_SERVER,
+    database: process.env.SQL_DB1,
+    driver: 'msnodesqlv8',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+      },
+    options: {
+        trustedconnection: true,
+        enableArithAort: true,
+    },
+    port: process.env.SQL_PORT1
+}
+
+module.exports = configJobData;
