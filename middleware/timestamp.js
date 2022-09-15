8ed409dda7e99392db4af2e54b6f8ad9ddfbe498
Kraig Piercy <kapiercy11@gmail.com>
2022-09-15 00:12:47 -0400

    add timestamp MW for logging use

---
+++ middleware/timestamp.js
@@ -0,0 +1,24 @@
+
+const getStamp = async (req, res) => {
+    try {
+        const d = new Date()
+        let year = d.getFullYear()
+        let month = d.getMonth() + 1
+        let day = d.getDate()
+        let hour = d.getHours()
+        let min = d.getMinutes()
+        let s = d.getSeconds()
+        let ms = d.getMilliseconds()
+        const stamp = month +"-"+ day +"-"+ year +" "+ hour +":" + min + ":" + s + "." + ms
+
+        return stamp
+        
+    } catch (e) {
+        console.log(e)
+        //throw new Error(e)
+    }
+};
+
+module.exports = {
+    getStamp
+}
