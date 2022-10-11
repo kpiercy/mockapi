5934f18233293c21a245432b759a1d978cfd3a92
Kraig Piercy <kapiercy11@gmail.com>
2022-10-11 10:25:55 -0400

    remove pool2 add logging

--- middleware/access.js
+++ middleware/access.js
@@ -4,30 +4,33 @@ const sql = require('mssql/msnodesqlv8')
 const configJobData = require('../config/JobData_dbconfig')
 
 async function apiAccess(req, res, next) {
-        let y = req.headers["authorization"];
-        if (y == null) {
-          y = req.body.token;
-        } else {
-          const authHeader = req.headers["authorization"];
-          y = authHeader && authHeader.split(" ")[1];
-        }
+      
+    let y = req.headers["authorization"];
+    if (y == null) {
+      y = req.body.token;
+    } else {
+      const authHeader = req.headers["authorization"];
+      y = authHeader && authHeader.split(" ")[1];
+    }
 
-        const token = y;
+    const token = y;
+
+    try{
 
         if (token == null) return res.status(401);
 
-    try{
         let pool = await sql.connect(configJobData)
         let canAccess = await pool.request()
             .input('token', sql.VarChar, token)
             .execute('UserAccessAPI')
             var thisUserAccess = canAccess.recordset[0].apiaccess
             var thisUserClient = canAccess.recordset[0].clientid
-        let pool2 = await sql.connect(configJobData)
+        await sql.connect(configJobData)
         let clientAccess = await pool.request()
             .input('clientid', sql.VarChar, thisUserClient)
             .execute('ClientAccessAPI')
             var thisClientAccess = clientAccess.recordset[0].Status
+        console.log({ userAccess: thisUserAccess, clientStatus: thisClientAccess })
         if (thisUserAccess === 'true' && thisClientAccess === 'Active') {
             console.log('***User & Client apiAccess: verified***')
             next()
