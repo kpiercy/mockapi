fa33803bf36ff809ec9d53f3e43bbf1b14a08dff
Kraig Piercy <kapiercy11@gmail.com>
2022-10-11 12:54:24 -0400

    update dowload controllers for production

--- controllers/dbops_downloads.js
+++ controllers/dbops_downloads.js
@@ -12,7 +12,7 @@ const all_downloads = async (req,res) => {
        let getDLs = await pool
          .request()
          .input("jobid", sql.NVarChar, req.params.jobid)
-         .execute("PostDownloads");
+         .execute("GetDownloads");
 
        res
          .status(200)
@@ -34,7 +34,7 @@ const one_download = async (req,res) => {
       let getDL = await pool
         .request()
         .input("downloadid", sql.NVarChar, req.params.downloadid)
-        .execute("PostDownloads");
+        .execute("GetDownload");
 
       res
         .status(200)
@@ -56,15 +56,13 @@ const create_download = async (req,res) => {
          let pool = await sql.connect(configJobData);
          let postDL = await pool
            .request()
-           .input("postDL", sql.NVarChar, downloads)
+           .input("downloads", sql.NVarChar, downloads)
            .execute("PostDownloads");
 
          res
            .status(200)
            .json(
-             JSON.parse(
-               postDL.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
-             )
+               postDL.recordsets
            );
        } catch (e) {
          res.status(500).json({ Error: e.message });
@@ -80,7 +78,7 @@ const delete_download = async (req,res) => {
             .input('downloadid', sql.VarChar, req.params.downloadid)
             .execute('DeleteDownload')
     
-        res.status(200).json(JSON.parse(deleted.recordset[0]['JSON_F52E2B61-18A1-11d1-B105-00805F49916B']))
+        res.status(200).json(deleted.recordsets)
     } catch (e){
         res.status(500).json({ Error: e.message })
         console.log(e);
