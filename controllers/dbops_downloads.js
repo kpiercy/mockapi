75e081fb89dc2889b6c0b4544b1cf3cb524a23c1
Kraig Piercy <kapiercy11@gmail.com>
2022-10-11 10:50:12 -0400

    add base logic for all download controllers

--- controllers/dbops_downloads.js
+++ controllers/dbops_downloads.js
@@ -6,27 +6,85 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const all_downloads = async (req,res) => {
-    console.log('dbops_downloads.all_downloads was reached')
-    console.log('Clientid used: '+req.params.clientid)
-    console.log('Jobid used: '+req.params.jobid)
+
+     try {
+       let pool = await sql.connect(configJobData);
+       let getDLs = await pool
+         .request()
+         .input("jobid", sql.NVarChar, req.params.jobid)
+         .execute("PostDownloads");
+
+       res
+         .status(200)
+         .json(
+           JSON.parse(
+             getDLs.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+           )
+         );
+     } catch (e) {
+       res.status(500).json({ Error: e.message });
+       console.log(e);
+     }
 }
 
 const one_download = async (req,res) => {
-    console.log('dbops_downloads.one_download was reached')
-    console.log('Clientid used: '+req.params.clientid)
-    console.log('Jobid used: '+req.params.jobid)
+    
+    try {
+      let pool = await sql.connect(configJobData);
+      let getDL = await pool
+        .request()
+        .input("downloadid", sql.NVarChar, req.params.downloadid)
+        .execute("PostDownloads");
+
+      res
+        .status(200)
+        .json(
+          JSON.parse(
+            getDL.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+          )
+        );
+    } catch (e) {
+      res.status(500).json({ Error: e.message });
+      console.log(e);
+    }
 }
 
 const create_download = async (req,res) => {
-    console.log('dbops_downloads.create_download was reached')
-    console.log('Clientid used: '+req.params.clientid)
-    console.log('Jobid used: '+req.params.jobid)
+    
+       try {
+        const downloads = JSON.stringify(req.body);
+         let pool = await sql.connect(configJobData);
+         let postDL = await pool
+           .request()
+           .input("postDL", sql.NVarChar, downloads)
+           .execute("PostDownloads");
+
+         res
+           .status(200)
+           .json(
+             JSON.parse(
+               postDL.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+             )
+           );
+       } catch (e) {
+         res.status(500).json({ Error: e.message });
+         console.log(e);
+       }
 }
 
 const delete_download = async (req,res) => {
-    console.log('dbops_downloads.delete_download was reached')
-    console.log('Clientid used: '+req.params.clientid)
-    console.log('Jobid used: '+req.params.jobid)
+
+    try{
+        let pool = await sql.connect(configJobData);
+        let deleted = await pool.request()
+            .input('downloadid', sql.VarChar, req.params.downloadid)
+            .execute('DeleteDownload')
+    
+        res.status(200).json(JSON.parse(deleted.recordset[0]['JSON_F52E2B61-18A1-11d1-B105-00805F49916B']))
+    } catch (e){
+        res.status(500).json({ Error: e.message })
+        console.log(e);
+    }
 }
 
 module.exports = {
