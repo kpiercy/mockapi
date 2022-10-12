94ca7416ad97e75644aa7bde22e0cbc0d2890fcd
Kraig Piercy <kapiercy11@gmail.com>
2022-10-12 14:15:11 -0400

    renamed payments.js to orbipays.js

--- routes/orbipays.js
+++ routes/orbipays.js
@@ -11,7 +11,7 @@ const checkReach = require('../middleware/reachlimiter')
 //child routes
 
 //controller
-const dboperations = require('../controllers/dbops_payments')
+const dboperations = require('../controllers/dbops_orbipays')
 
 //model
 
