32e55477d46b7702a6fad62a2b9cfa383142bfce
Kraig Piercy <kapiercy11@gmail.com>
2022-10-21 00:57:03 -0400

    add /clients/jobs/facilities route

---
+++ controllers/dbops_facilities.js
@@ -0,0 +1,111 @@
+require("dotenv").config();
+
+var configJobData = require("../config/JobData_dbconfig");
+const sql = require("mssql/msnodesqlv8");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const all_facilities = async (req, res) => {
+  try {
+    const jobid = req.params.jobid;
+    let pool = await sql.connect(configJobData);
+    let getFacilities = await pool
+      .request()
+      .input("jobid", sql.NVarChar, jobid.toLowerCase())
+      .execute("GetFacilities");
+
+    res
+      .status(200)
+      .json(
+        JSON.parse(
+          getFacilities.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+        )
+      );
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+const one_facility = async (req, res) => {
+  try {
+    const facilityid = req.params.facilityid;
+    let pool = await sql.connect(configJobData);
+    let getFacility = await pool
+      .request()
+      .input("facilityid", sql.NVarChar, facilityid.toLowerCase())
+      .execute("GetFacility");
+
+    res
+      .status(200)
+      .json(
+        JSON.parse(
+          getFacility.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+        )
+      );
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//updates fields to values provided or leaves field value as is if field is not provided in req.body, will also create a record if one is not found
+const update_facility = async (req, res) => {
+  try {
+    const facilities = JSON.stringify(req.body);
+    const facilityid = req.params.facilityid;
+    let pool = await sql.connect(configJobData);
+    let putFacilities = await pool
+      .request()
+      .input("facilities", sql.NVarChar, facilities)
+      .input("facilityid", sql.NVarChar, facilityid.toLowerCase())
+      .execute("PutFacilities");
+
+    res.status(200).json(putFacilities.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//create facility
+const create_facility = async (req, res) => {
+  try {
+    const facilities = JSON.stringify(req.body);
+    let pool = await sql.connect(configJobData);
+    let postFacilities = await pool
+      .request()
+      .input("facilities", sql.NVarChar, facilities)
+      .execute("PostFacilities");
+
+    res.status(200).json(postFacilities.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//deactivate facility
+const delete_facility = async (req, res) => {
+  try {
+    const facilityid = req.params.facilityid;
+    let pool = await sql.connect(configJobData);
+    let delFacility = await pool
+      .request()
+      .input("facilityid", sql.NVarChar, facilityid.toLowerCase())
+      .execute("DeleteFacility");
+
+    res.status(200).json(delFacility.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+module.exports = {
+  all_facilities,
+  one_facility,
+  update_facility,
+  create_facility,
+  delete_facility,
+};
---
+++ routes/facilities.js
@@ -0,0 +1,37 @@
+require("dotenv").config();Credit
+
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const pubip = require("express-ip");
+
+//additional middleware
+const authLvl = require("../middleware/authLvl");
+const checkReach = require("../middleware/reachlimiter");
+
+//child routes
+
+//controller
+const dboperations = require("../controllers/dbops_facilities");
+
+//model
+
+//router options and children
+router.use(pubip().getIpInfoMiddleware);
+//router.all('*', publimiter, authenticateToken, authAccess, authIP) //instantiated by clients parent router and called once url is reconciled
+
+//get all facilities for this job
+router.get("/", checkReach, dboperations.all_facilities);
+
+//get single facilitie for this job by id
+router.get("/:facilityid", checkReach, dboperations.one_facility);
+
+//update single facilitie for this job by id
+router.put("/:facilityid", checkReach, dboperations.update_facility);
+
+//create new facilitie by job
+router.post("/", checkReach, dboperations.create_facility);
+
+//delete facilitie for this job
+router.delete("/:facilityid", checkReach, authLvl, dboperations.delete_facility);
+
+module.exports = router;
