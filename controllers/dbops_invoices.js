f384f5bd01e036a6944c011adb0e554d322e31da
Kraig Piercy <kapiercy11@gmail.com>
2022-10-21 00:51:44 -0400

    add /clients/invoices route

--- controllers/dbops_invoices.js
+++ controllers/dbops_invoices.js
@@ -1,32 +1,111 @@
-require('dotenv').config()
+require("dotenv").config();
 
-var configJobData = require('../config/JobData_dbconfig')
-const sql = require('mssql/msnodesqlv8')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+var configJobData = require("../config/JobData_dbconfig");
+const sql = require("mssql/msnodesqlv8");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const all_invoices = async (req,res) => {
-    console.log('dbops_invoices.all_invoices was reached')
-}
+const all_invoices = async (req, res) => {
+  try {
+    const jobid = req.params.jobid;
+    let pool = await sql.connect(configJobData);
+    let getInvoices = await pool
+      .request()
+      .input("jobid", sql.NVarChar, jobid.toLowerCase())
+      .execute("GetInvoices");
 
-const one_invoice = async (req,res) => {
-    console.log('dbops_invoices.one_invoice was reached')
-    console.log('Invoice id used:'+req.params.jobid)
-}
+    res
+      .status(200)
+      .json(
+        JSON.parse(
+          getInvoices.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+        )
+      );
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
 
-const create_invoice = async (req,res) => {
-    console.log('dbops_invoices.create_invoice was reached')
-    console.log('Invoice id used:'+req.params.jobid)
-}
+const one_invoice = async (req, res) => {
+  try {
+    const invoiceid = req.params.invoiceid;
+    let pool = await sql.connect(configJobData);
+    let getInvoice = await pool
+      .request()
+      .input("invoiceid", sql.NVarChar, invoiceid.toLowerCase())
+      .execute("GetInvoice");
 
-const delete_invoice = async (req,res) => {
-    console.log('dbops_invoices.delete_invoice was reached')
-    console.log('Invoice id used:'+req.params.jobid)
-}
+    res
+      .status(200)
+      .json(
+        JSON.parse(
+          getInvoice.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+        )
+      );
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//updates fields to values provided or leaves field value as is if field is not provided in req.body, will also create a record if one is not found
+const update_invoice = async (req, res) => {
+  try {
+    const invoices = JSON.stringify(req.body);
+    const invoiceid = req.params.invoiceid;
+    let pool = await sql.connect(configJobData);
+    let getInvoices = await pool
+      .request()
+      .input("invoices", sql.NVarChar, invoices)
+      .input("invoiceid", sql.NVarChar, invoiceid.toLowerCase())
+      .execute("PutInvoices");
+
+    res.status(200).json(getInvoices.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//create invoice
+const create_invoice = async (req, res) => {
+  try {
+    const invoices = JSON.stringify(req.body);
+    let pool = await sql.connect(configJobData);
+    let postInvoices = await pool
+      .request()
+      .input("invoices", sql.NVarChar, invoices)
+      .execute("PostInvoices");
+
+    res.status(200).json(postInvoices.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+//deactivate invoice
+const delete_invoice = async (req, res) => {
+  try {
+    const invoiceid = req.params.invoiceid;
+    let pool = await sql.connect(configJobData);
+    let delInvoice = await pool
+      .request()
+      .input("invoiceid", sql.NVarChar, invoiceid.toLowerCase())
+      .execute("DeleteInvoice");
+
+    res.status(200).json(delInvoice.recordsets);
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
 
 module.exports = {
-    all_invoices,
-    one_invoice,
-    create_invoice,
-    delete_invoice
-}
+  all_invoices,
+  one_invoice,
+  update_invoice,
+  create_invoice,
+  delete_invoice,
+};
--- routes/invoices.js
+++ routes/invoices.js
@@ -9,6 +9,8 @@ const authLvl = require('../middleware/authLvl')
 const checkReach = require('../middleware/reachlimiter')
 
 //child routes
+const creditRoutes = require("./credits");
+const depositRoutes = require("./deposits");
 
 //controller
 const dboperations = require('../controllers/dbops_invoices')
@@ -18,17 +20,22 @@ const dboperations = require('../controllers/dbops_invoices')
 //router options and children 
 router.use(pubip().getIpInfoMiddleware)
 //router.all('*', publimiter, authenticateToken, authAccess, authIP) //instantiated by clients parent router and called once url is reconciled
+router.use("/:invoiceid/credits", creditRoutes);
+router.use("/:invoiceid/deposits", depositRoutes);
 
 //get all invoices, paginate
 router.get('/', checkReach, dboperations.all_invoices)
 
 //get single invoice by id
-router.get('/:id', checkReach, dboperations.one_invoice)
+router.get('/:invoiceid', checkReach, dboperations.one_invoice)
+
+//update single deposit for this job by id
+router.put("/:invoiceid", checkReach, dboperations.update_invoice);
 
 //create new invoice
 router.post('/', checkReach, authLvl, dboperations.create_invoice)
 
 //delete invoice
-router.delete('/', checkReach, authLvl, dboperations.delete_invoice)
+router.delete('/:invoiceid', checkReach, authLvl, dboperations.delete_invoice)
 
 module.exports = router;
