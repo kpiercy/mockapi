6ad1676aaed3fd90201cb9f2606763a55c4710f4
Kraig Piercy <kapiercy11@gmail.com>
2022-10-25 17:31:19 -0400

    add middleware protections to static routes

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 
+//middleware
+const publimiter = require('./middleware/publimiter')
+const authenticateToken = require('./middleware/authToken')
+const authLvl = require('./middleware/authLvl') // pass on routes that can only be hit internally
+const authAccess = require('./middleware/access')
+const authIP = require('./middleware/ipAccess')
+const checkReach = require('./middleware/reachlimiter') //pass on routes that need query reach limited to only their client (bypasses check if user is internal)
+
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 
 const fileRoutes = require('./routes/files')
-//const versfileRoutes = require('./routes/versfiles')
 const clientRoutes = require('./routes/clients')
 const serviceRoutes = require("./routes/services");
 const invoiceRoutes = require("./routes/invoices");
@@ -55,20 +63,104 @@ Static routes are handled via the list of app.use below, whereby it calls the gi
 All of the same capabilities are open to Static and Dynamic requests. The difference being that when using the static routes, you will need to provide details like the clientid, orderid, ... params via JSON body in most cases. 
 */
  
-app.use('/api/v1/clients', clientRoutes)
-app.use("/api/v1/services", serviceRoutes);
-app.use('/api/v1/clients/invoices', invoiceRoutes)
-app.use('/api/v1/clients/invoices/credits', creditRoutes)
-app.use('/api/v1/clients/invoices/deposits', depositRoutes)
-app.use('/api/v1/clients/contracts', contractRoutes)
-app.use('/api/v1/clients/contracts/prices', priceRoutes)
-app.use('/api/v1/clients/jobs', jobRoutes)
-app.use('/api/v1/clients/jobs/proofs', proofRoutes)
-app.use('/api/v1/clients/jobs/downloads', downloadRoutes) //crud done
-app.use('/api/v1/clients/jobs/contacts', contactRoutes) //crud done
-app.use('/api/v1/clients/jobs/orbipays', orbipayRoutes) //crud done
-app.use('/api/v1/clients/jobs/orders', orderRoutes) //cru done
-app.use('/api/v1/clients/jobs/facilities', facilityRoutes) 
+app.use("/api/v1/clients",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP, 
+  clientRoutes
+); //crud
+app.use("/api/v1/services",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  serviceRoutes
+); //cru 
+app.use("/api/v1/clients/invoices",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP, 
+  invoiceRoutes
+); //cru
+app.use("/api/v1/clients/invoices/credits",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP, 
+  creditRoutes
+); //cru
+app.use("/api/v1/clients/invoices/deposits",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  depositRoutes
+); //cru
+app.use("/api/v1/clients/contracts",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP, 
+  contractRoutes
+); //crud
+app.use("/api/v1/clients/contracts/prices",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  priceRoutes
+); //cru
+app.use("/api/v1/clients/jobs",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  jobRoutes
+);
+app.use("/api/v1/clients/jobs/proofs",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  proofRoutes
+);
+app.use("/api/v1/clients/jobs/downloads",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  downloadRoutes
+); //crud
+app.use("/api/v1/clients/jobs/contacts",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  contactRoutes
+); //crud
+app.use("/api/v1/clients/jobs/orbipays",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  orbipayRoutes
+); //crud
+app.use("/api/v1/clients/jobs/orders",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  orderRoutes
+); //cru
+app.use("/api/v1/clients/jobs/facilities",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  facilityRoutes
+); 
 // app.use('/api/v1/clients/jobs/orders/versions', versionRoutes)
 // app.use('/api/v1/clients/jobs/orders/versions/files', fileRoutes)  //should files belong to /orders instead of /versions??
 // app.use('/api/v1/clients/jobs/orders/versions/files/inserts', insertRoutes) //should inserts belong to /jobs/orders/files??
