bf8c9937def3fce4f1d501859fa7b0f2f220bf4a
Kraig Piercy <kapiercy11@gmail.com>
2022-11-01 23:42:45 -0400

    add pagination to get /orders route

--- controllers/dbops_orders.js
+++ controllers/dbops_orders.js
@@ -5,27 +5,97 @@ const sql = require("mssql/msnodesqlv8");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+//classes
+const model = require('../classes/order')
+
 //get all orders for client by jobid
 const all_orders = async (req, res) => {
-  try {
-    const jobid = req.params.jobid;
-    let pool = await sql.connect(configJobData);
-    let getOrders = await pool
-      .request()
-      .input("jobid", sql.NVarChar, jobid.toLowerCase())
-      .execute("GetOrders");
+  req.jobid = req.params.jobid
+  req.params.jobid = req.jobid
+  let jid = req.params.jobid
+  req.clientid = req.params.clientid
+  req.params.clientid = req.clientid
+  let cid = req.params.clientid
+  let pageIt = req.query.paginate
 
-    res
-      .status(200)
-      .json(
-        JSON.parse(
-          getOrders.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
-        )
-      );
-  } catch (e) {
-    res.status(500).json({ Error: e.message });
-    console.log(e);
-  }
+  if ( pageIt === 'true' ) {
+      const page = parseInt(req.query.page)
+      const limit = parseInt(req.query.limit)
+      const startIndex = (page - 1) * limit
+      const endIndex = page * limit
+
+      if (jid.toLowerCase() == null) {
+        res
+          .status(406)
+          .json(
+            {Error: "jobid must be specified in either the URL as a query param or in the request body."}
+          );
+      } else {
+        const results = {};
+
+        if (endIndex < model.length) {
+          let nextPage = page + 1;
+          results.next =
+            "http://localhost:5000/clients/" + cid.toLowerCase() + "/jobs/" + jid.toLowerCase() + "/orders?paginate=true&page=" +
+            nextPage +
+            "&limit=" +
+            limit +
+            "";
+        }
+        if (startIndex > 0) {
+          let prevPage = page - 1;
+          results.previous =
+            "http://localhost:5000/clients/" + cid.toLowerCase() + "/jobs/" + jid.toLowerCase() + "/orders?paginate=true&page=" +
+            prevPage +
+            "&limit=" +
+            limit +
+            "";
+        }
+        try {
+          let pool = await sql.connect(configJobData);
+          results.data = await pool
+            .request()
+            .input("startindex", sql.Int, startIndex)
+            .input("limit", sql.Int, limit)
+            .input("jid", sql.VarChar, jid.toLowerCase())
+            .execute("GetPaginatedOrders");
+          res.paginatedResults = results;
+          res
+            .status(200)
+            .json(
+              {
+                Next: res.paginatedResults.next,
+                Previous: res.paginatedResults.previous,
+                Orders: res.paginatedResults.data.recordset
+              }
+            );
+          //res.paginatedResults
+        } catch (e) {
+          console.log(e);
+          res.status(500).json({ Error: e.message });
+        }
+      }
+  } else {
+      try {
+        const jobid = req.params.jobid;
+        let pool = await sql.connect(configJobData);
+        let getOrders = await pool
+          .request()
+          .input("jobid", sql.NVarChar, jobid.toLowerCase())
+          .execute("GetOrders");
+
+        res
+          .status(200)
+          .json(
+            JSON.parse(
+              getOrders.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+            )
+          );
+      } catch (e) {
+        res.status(500).json({ Error: e.message });
+        console.log(e);
+      }
+    }
 };
 
 //get one order by orderid
