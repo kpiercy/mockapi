882a7f1b98e92ce1f0f27937946ea279faeaf4dd
Kraig Piercy <kapiercy11@gmail.com>
2022-11-02 22:28:18 -0400

    pretty server.js

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const pubip = require("express-ip")
-const logger = require('morgan')
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const pubip = require("express-ip");
+const logger = require("morgan");
 
 //middleware
-const publimiter = require('./middleware/publimiter')
-const authenticateToken = require('./middleware/authToken')
-const authAccess = require('./middleware/access')
-const authIP = require('./middleware/ipAccess')
+const publimiter = require("./middleware/publimiter");
+const authenticateToken = require("./middleware/authToken");
+const authAccess = require("./middleware/access");
+const authIP = require("./middleware/ipAccess");
 
 // app.set("views", "views");
 // app.set("view engine", "ejs");
-app.use(express.json())
-app.use(cors())
-app.use(express.static('public'))
-app.use(pubip().getIpInfoMiddleware)
-app.use(logger('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+app.use(pubip().getIpInfoMiddleware);
+app.use(logger("dev"));
 
-const indexRoutes = require('./routes/index')
-const fileRoutes = require('./routes/files')
-const clientRoutes = require('./routes/clients')
+const indexRoutes = require("./routes/index");
+const fileRoutes = require("./routes/files");
+const clientRoutes = require("./routes/clients");
 const serviceRoutes = require("./routes/services");
 const invoiceRoutes = require("./routes/invoices");
 const creditRoutes = require("./routes/credits");
 const depositRoutes = require("./routes/deposits");
 const contractRoutes = require("./routes/contracts");
 const priceRoutes = require("./routes/prices");
-const jobRoutes = require('./routes/jobs')
+const jobRoutes = require("./routes/jobs");
 const facilityRoutes = require("./routes/facilities");
 const proofRoutes = require("./routes/proofs");
-const downloadRoutes = require('./routes/downloads')
-const contactRoutes = require('./routes/contacts')
-const orbipayRoutes = require('./routes/orbipays')
-const orderRoutes = require('./routes/orders')
-const versionRoutes = require('./routes/versions')
-const insertRoutes = require('./routes/inserts')
-const messageRoutes = require("./routes/messages")
-const logoRoutes = require("./routes/logos")
-const patientRoutes = require('./routes/patients')
-const encounterRoutes = require('./routes/encounters')
-const detailRoutes = require('./routes/details')
+const downloadRoutes = require("./routes/downloads");
+const contactRoutes = require("./routes/contacts");
+const orbipayRoutes = require("./routes/orbipays");
+const orderRoutes = require("./routes/orders");
+const versionRoutes = require("./routes/versions");
+const insertRoutes = require("./routes/inserts");
+const messageRoutes = require("./routes/messages");
+const logoRoutes = require("./routes/logos");
+const patientRoutes = require("./routes/patients");
+const encounterRoutes = require("./routes/encounters");
+const detailRoutes = require("./routes/details");
 
 ///////////////endpoint routes ////////////////
 
@@ -55,33 +55,118 @@ All of the same capabilities are open to Static and Dynamic requests. The differ
 */
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Origin-Allow-Headers', 
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Origin-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
 
-    next()
-})
+  next();
+});
 
- 
 app.use("/", indexRoutes);
 app.use("/api/v1/clients", clientRoutes); //crud
-app.use("/api/v1/services", serviceRoutes); //cru 
-app.use("/api/v1/clients/invoices", publimiter, authenticateToken, authAccess, authIP, invoiceRoutes); //cru
-app.use("/api/v1/clients/invoices/credits", publimiter, authenticateToken, authAccess, authIP, creditRoutes); //cru
-app.use("/api/v1/clients/invoices/deposits", publimiter, authenticateToken, authAccess, authIP, depositRoutes); //cru
-app.use("/api/v1/clients/contracts", publimiter, authenticateToken, authAccess, authIP, contractRoutes); //crud
-app.use("/api/v1/clients/contracts/prices", publimiter, authenticateToken, authAccess, authIP, priceRoutes); //cru
-app.use("/api/v1/clients/jobs", publimiter, authenticateToken, authAccess, authIP, jobRoutes);
-app.use("/api/v1/clients/jobs/proofs", publimiter, authenticateToken, authAccess, authIP, proofRoutes);
-app.use("/api/v1/clients/jobs/downloads", publimiter, authenticateToken, authAccess, authIP, downloadRoutes); //crud
-app.use("/api/v1/clients/jobs/contacts", publimiter, authenticateToken, authAccess, authIP, contactRoutes); //crud
-app.use("/api/v1/clients/jobs/orbipays", publimiter, authenticateToken, authAccess, authIP, orbipayRoutes); //crud
-app.use("/api/v1/clients/jobs/orders", publimiter, authenticateToken, authAccess, authIP, orderRoutes); //cru
-app.use("/api/v1/clients/jobs/facilities", publimiter, authenticateToken, authAccess, authIP, facilityRoutes); 
+app.use("/api/v1/services", serviceRoutes); //cru
+app.use(
+  "/api/v1/clients/invoices",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  invoiceRoutes
+); //cru
+app.use(
+  "/api/v1/clients/invoices/credits",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  creditRoutes
+); //cru
+app.use(
+  "/api/v1/clients/invoices/deposits",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  depositRoutes
+); //cru
+app.use(
+  "/api/v1/clients/contracts",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  contractRoutes
+); //crud
+app.use(
+  "/api/v1/clients/contracts/prices",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  priceRoutes
+); //cru
+app.use(
+  "/api/v1/clients/jobs",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  jobRoutes
+);
+app.use(
+  "/api/v1/clients/jobs/proofs",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  proofRoutes
+);
+app.use(
+  "/api/v1/clients/jobs/downloads",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  downloadRoutes
+); //crud
+app.use(
+  "/api/v1/clients/jobs/contacts",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  contactRoutes
+); //crud
+app.use(
+  "/api/v1/clients/jobs/orbipays",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  orbipayRoutes
+); //crud
+app.use(
+  "/api/v1/clients/jobs/orders",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  orderRoutes
+); //cru
+app.use(
+  "/api/v1/clients/jobs/facilities",
+  publimiter,
+  authenticateToken,
+  authAccess,
+  authIP,
+  facilityRoutes
+);
 // app.use('/api/v1/clients/jobs/orders/versions', versionRoutes)
 // app.use('/api/v1/clients/jobs/orders/versions/files', fileRoutes)  //should files belong to /orders instead of /versions??
 // app.use('/api/v1/clients/jobs/orders/versions/files/inserts', insertRoutes) //should inserts belong to /jobs/orders/files??
@@ -93,26 +178,24 @@ app.use("/api/v1/clients/jobs/facilities", publimiter, authenticateToken, authAc
 //api/v1/clients/jobs/facilities/statements/encounters/charges/adjustments
 //api/v1/clients/jobs/facilities/statements/encounters/charges/transfers
 
-
 ///////////////endpoint routes////////////////
 
 app.use((req, res, next) => {
-    const error = new Error('Not found')
-    error.status = 404
-    next(error)
-})
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-    next()
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+  next();
+});
 
-
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server is running at port ' + port)
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log("server is running at port " + port);
