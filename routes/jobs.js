27cf20bd23f76a510f7d0fbb30c97646f94910f5
Kraig Piercy <kapiercy11@gmail.com>
2022-11-11 16:28:13 -0500

    add /facilities and /orders subrouting to /jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -14,10 +14,11 @@ const downloadRoutes = require('./downloads')
 const returnRoutes = require('./returns')
 const contactRoutes = require('./contacts')
 const orbipayRoutes = require('./orbipays')
-const orderRoutes = require('./orders')
 const proofRoutes = require('./proofs')
 const processRoutes = require('./processes')
 const workflowRoutes = require("./workflows");
+const facilityRoutes = require('./facilities')
+const orderRoutes = require("./orders");
 
 //controller
 const dboperations = require('../controllers/jobs')
@@ -27,12 +28,13 @@ const dboperations = require('../controllers/jobs')
 //router options and children
 router.use(pubip().getIpInfoMiddleware)
 //router.all('*', publimiter, authenticateToken, authAccess, authIP) //instantiated by clients parent router and called once url is reconciled
+router.use('/:jobid/facilities', facilityRoutes)
+router.use("/:jobid/orders", orderRoutes);
 router.use('/:jobid/files', fileRoutes)
 router.use('/:jobid/downloads', downloadRoutes)
 router.use('/:jobid/returns', returnRoutes)
 router.use('/:jobid/contacts', contactRoutes)
-router.use('/:jobid/orders', orderRoutes)
-router.use('/:jobid/orbipays', orbipayRoutes)
+router.use('/:jobid/orbipays', orbipayRoutes) //convert to be a job of Client rather than standalone route
 router.use('/:jobid/proofs', proofRoutes)
 router.use("/:jobid/processes", processRoutes);
 router.use("/:jobid/workflows", workflowRoutes);
