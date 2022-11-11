5fd076cd148696f95cc39066f676b24add1114cf
Kraig Piercy <kapiercy11@gmail.com>
2022-11-11 16:27:18 -0500

    add /reports route

---
+++ controllers/reports.js
@@ -0,0 +1,187 @@
+require("dotenv").config();
+
+const configJobData = require("../config/JobData_dbconfig");
+const sql = require("mssql/msnodesqlv8");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//classes
+// const model = require("../classes/report");
+
+// const all_reports = async (req, res) => {
+//   req.jobid = req.params.jobid;
+//   req.params.jobid = req.jobid;
+//   let jid = req.params.jobid;
+//   req.clientid = req.params.clientid;
+//   req.params.clientid = req.clientid;
+//   let cid = req.params.clientid;
+//   let pageIt = req.query.paginate;
+
+//   if (pageIt === "true") {
+//     const page = parseInt(req.query.page);
+//     const limit = parseInt(req.query.limit);
+//     const startIndex = (page - 1) * limit;
+//     const endIndex = page * limit;
+
+//     if (jid.toLowerCase() == null) {
+//       res
+//         .status(406)
+//         .json({
+//           Error:
+//             "jobid must be reportified in either the URL as a query param or in the request body.",
+//         });
+//     } else {
+//       const results = {};
+
+//       if (endIndex < model.length) {
+//         let nextPage = page + 1;
+//         results.next =
+//           "http://localhost:5000/clients/" +
+//           cid.toLowerCase() +
+//           "/jobs/" +
+//           jid.toLowerCase() +
+//           "/facilities/" +
+//           fid.toLowerCase() +
+//           "/reports?paginate=true&page=" +
+//           nextPage +
+//           "&limit=" +
+//           limit +
+//           "";
+//       }
+//       if (startIndex > 0) {
+//         let prevPage = page - 1;
+//         results.previous =
+//           "http://localhost:5000/clients/" +
+//           cid.toLowerCase() +
+//           "/jobs/" +
+//           jid.toLowerCase() +
+//           "/facilities/" +
+//           fid.toLowerCase() +
+//           "/reports?paginate=true&page=" +
+//           prevPage +
+//           "&limit=" +
+//           limit +
+//           "";
+//       }
+//       try {
+//         let pool = await sql.connect(configJobData);
+//         results.data = await pool
+//           .request()
+//           .input("startindex", sql.Int, startIndex)
+//           .input("limit", sql.Int, limit)
+//           .input("jid", sql.VarChar, jid.toLowerCase())
+//           .execute("GetPaginatedReports");
+//         res.paginatedResults = results;
+//         res.status(200).json({
+//           Next: res.paginatedResults.next,
+//           Previous: res.paginatedResults.previous,
+//           Reports: res.paginatedResults.data.recordset,
+//         });
+//         //res.paginatedResults
+//       } catch (e) {
+//         console.log(e);
+//         res.status(500).json({ Error: e.message });
+//       }
+//     }
+//   } else {
+//     try {
+//       const jobid = req.params.jobid;
+//       let pool = await sql.connect(configJobData);
+//       let getReports = await pool
+//         .request()
+//         .input("jobid", sql.NVarChar, jobid.toLowerCase())
+//         .execute("GetReports");
+
+//       res
+//         .status(200)
+//         .json(
+//           JSON.parse(
+//             getReports.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+//           )
+//         );
+//     } catch (e) {
+//       res.status(500).json({ Error: e.message });
+//       console.log(e);
+//     }
+//   }
+// };
+
+const one_report = async (req, res) => {
+  try {
+    const reportid = req.params.reportid;
+    let pool = await sql.connect(configJobData);
+    let getReport = await pool
+      .request()
+      .input("reportid", sql.NVarChar, reportid.toLowerCase())
+      .execute("GetReport");
+
+    res
+      .status(200)
+      .json(
+        JSON.parse(
+          getReport.recordset[0]["JSON_F52E2B61-18A1-11d1-B105-00805F49916B"]
+        )
+      );
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+const create_report = async (req, res) => {
+  try {
+    const reports = JSON.stringify(req.body);
+    let pool = await sql.connect(configJobData);
+    let postReport = await pool
+      .request()
+      .input("reports", sql.NVarChar, reports)
+      .execute("PostReports");
+
+    res.status(201).json({ Reports: postReport.recordset });
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+const update_report = async (req, res) => {
+  try {
+    const reportid = req.params.reportid;
+    const reports = JSON.stringify(req.body);
+    let pool = await sql.connect(configJobData);
+    let putReport = await pool
+      .request()
+      .input("reports", sql.NVarChar, reports)
+      .input("reportid", sql.VarChar, reportid.toLowerCase())
+      .execute("PutReports");
+
+    res.status(200).json({ Reports: putReport.recordset });
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+const delete_report = async (req, res) => {
+  try {
+    const reportid = req.params.reportid;
+    let pool = await sql.connect(configJobData);
+    let deleted = await pool
+      .request()
+      .input("reportid", sql.VarChar, reportid.toLowerCase())
+      .execute("DeleteReport");
+
+    res.status(200).json({ Reports: deleted.recordset });
+  } catch (e) {
+    res.status(500).json({ Error: e.message });
+    console.log(e);
+  }
+};
+
+module.exports = {
+  //all_reports,
+  one_report,
+  create_report,
+  update_report,
+  delete_report,
+};
---
+++ routes/reports.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const pubip = require("express-ip");
+
+//additional middleware
+const authLvl = require("../middleware/authLvl");
+const checkReach = require("../middleware/reachlimiter");
+
+//child routes
+
+//controller
+const dboperations = require("../controllers/reports");
+
+//model
+
+//router options and children
+router.use(pubip().getIpInfoMiddleware);
+//router.all('*', publimiter, authenticateToken, authAccess, authIP) //instantiated by clients parent router and called once url is reconciled
+
+//get all reports for this job
+//router.get("/", checkReach, dboperations.all_reports);
+
+//get single report for this job by id
+router.get("/:reportid", checkReach, dboperations.one_report);
+
+//update existing based on provided fields or create if not found
+router.patch("/:reportid", checkReach, dboperations.update_report);
+
+//create new report by job
+router.post("/", checkReach, authLvl, dboperations.create_report);
+
+//delete report for this job
+router.delete("/:reportid", checkReach, authLvl, dboperations.delete_report);
+
+module.exports = router;
