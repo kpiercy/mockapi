7ea0cfbeba3ff66ee457a22933f4278c01aeb455
Kraig Piercy <kapiercy11@gmail.com>
2022-11-22 16:01:45 -0500

    single quotes

--- routes/clients.js
+++ routes/clients.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 //const pubip = require('express-ip')
 
 //middleware
@@ -11,8 +11,8 @@ const authLvl = require('../middleware/authLvl') // pass on routes that can only
 const authAccess = require('../middleware/access')
 const authIP = require('../middleware/ipAccess')
 const checkReach = require('../middleware/reachlimiter') //pass on routes that need query reach limited to only their client (bypasses check if user is internal)
-const validateDto = require("../middleware/validateDto");
-const clientDto = require("../dto/clients");
+const validateDto = require('../middleware/validateDto')
+const clientDto = require('../dto/clients')
 
 //controller
 const dboperations = require('../controllers/clients')
@@ -20,12 +20,12 @@ const dboperations = require('../controllers/clients')
 //child routes
 const jobRoutes = require('./jobs')
 const invoiceRoutes = require('./invoices')
-const contractRoutes = require("./contracts");
+const contractRoutes = require('./contracts')
 
 //router options and children
 //router.use(pubip().getIpInfoMiddleware)
 router.all('*', publimiter, authenticateToken, authAccess, authIP)
-router.use("/:clientid/contracts", contractRoutes);
+router.use('/:clientid/contracts', contractRoutes)
 router.use('/:clientid/invoices', invoiceRoutes)
 router.use('/:clientid/jobs', jobRoutes)
 
@@ -36,12 +36,12 @@ router.post('/', authLvl, validateDto(clientDto), dboperations.clients_create)
 router.get('/:clientid', checkReach, dboperations.clients_client_all)
 
 //udpate client by clientid or all if providing parent_clientid
-//router.get('/:clientid', checkReach, dboperations.clients_client_mn) 
+//router.get('/:clientid', checkReach, dboperations.clients_client_mn)
 
 //update client with fields provided in json body
-router.patch('/:clientid', authLvl, dboperations.update_client) 
+router.patch('/:clientid', authLvl, dboperations.update_client)
 
 //set client and any children as inactive if parentclientid is provided
 router.delete('/', authLvl, dboperations.clients_delete)
 
-module.exports = router;
+module.exports = router
