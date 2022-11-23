08daf319eaf16425a8fec839e1b0a8c0803b7a6b
Kraig Piercy <kapiercy11@gmail.com>
2022-11-23 13:11:42 -0500

    update rate limiters

--- middleware/authlimiter.js
+++ middleware/authlimiter.js
@@ -1,8 +1,14 @@
 const rateLimit = require('express-rate-limit')
 const authlimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, //60 min
-    max: 25, //limit each IP to 5 requests per 60min
-    message: "429 : Too many requests from this IP in the last 60 min, please try again later."
+  windowMs: 60 * 60 * 1000, // 30 minutes
+  max: 25, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (_request, res, _next) =>
+    res.status(429).json({
+      status: false,
+      message: 'Too many requests, please try again later.',
+    }),
 })
 
 module.exports = authlimiter;
--- middleware/publimiter.js
+++ middleware/publimiter.js
@@ -1,8 +1,14 @@
 const rateLimit = require('express-rate-limit')
 const publimiter = rateLimit({
-    windowMs: 30 * 60 * 1000, //30 min
-    max: 300, //limit each IP to 100 requests per 30min
-    message: "429 : Too many requests from this IP in the last 30 min, please try again later."
+  windowMs: 30 * 60 * 1000, // 30 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (_request, res, _next) =>
+    res.status(429).json({
+      status: false,
+      message: 'Too many requests, please try again later.',
+    }),
 })
 
 module.exports = publimiter;
