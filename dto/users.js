c546f1d625541e74ee6b9464a1d54e3fb306b329
Kraig Piercy <kapiercy11@gmail.com>
2022-11-25 07:34:24 -0500

    add users dto for POST /users

--- dto/users.js
+++ dto/users.js
@@ -1,10 +1,14 @@
 const yup = require("yup");
 
 module.exports = yup.object().shape({
-  Client_GUID: yup.string().uuid().required(),
-  Email: yup.string().trim().required().email(),
-  Username: yup.string().trim().lowercase().required(),
-  Password: yup.string().min(16).required(),
-  ApiAccess: yup.boolean().default(false).required(),
-  AllowableIP: yup.string().trim()
+  Users: yup.array().of(
+    yup.object().shape({
+      Client_GUID: yup.string().uuid().required(),
+      Email: yup.string().trim().email().required(),
+      Username: yup.string().trim().lowercase().required(),
+      Password: yup.string().min(16).required(),
+      ApiAccess: yup.boolean().default(false).required(),
+      AllowableIP: yup.string().trim().required()
+    })
+  ),
 });
