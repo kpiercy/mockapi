bcd1fc9651f0f52d9620847dd0aea44165ed8969
Kraig Piercy <kapiercy11@gmail.com>
2022-12-20 08:43:40 -0500

    temporary increase to publimiter timeout

--- middleware/publimiter.js
+++ middleware/publimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit')
 const publimiter = rateLimit({
   windowMs: 30 * 60 * 1000, // 30 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 20 minutes)
+  max: 500, // Limit each IP to 500 requests per `window` (here, per 20 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: (_request, res, _next) =>
