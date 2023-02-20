6ee09a10b9f00f6c5f8dc7b201bc4ecf60ea368b
Kraig Piercy <kapiercy11@gmail.com>
2023-02-20 10:05:36 -0500

    add /charts swagger docs

--- routes/charts.js
+++ routes/charts.js
@@ -19,16 +19,144 @@ const dboperations = require("../controllers/charts");
 router.use(pubip().getIpInfoMiddleware);
 //router.all('*', publimiter, authenticateToken, authAccess, authIP) //instantiated by clients parent router and called once url is reconciled
 
-//GetOne
+ /**
+  * @swagger
+  * tags:
+  *   name: Charts
+  *   description: 
+  */
+
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/facilities/{facilityid}/specs/{specid}/charts/{chartid}:
+ *  get:
+ *      summary: Use to find chart by id
+ *      tags: [Charts]
+ *      description: Find a chart entry
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID of data to find
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to find
+ *        - in: path
+ *          name: facilityid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: FacilityID of data to find
+ *        - in: path
+ *          name: specid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: SpecID of data to find
+ *        - in: path
+ *          name: chartid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ChartID of data to find
+ *      responses:
+ *          202:
+ *              description: Found chart
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Charts'
+ *          404:
+ *              description: Chart record was not found
+ */
 router.get("/:chartid", checkReach, dboperations.one_chart);
 
 //PatchOne
 router.patch("/:chartid", checkReach, dboperations.update_chart);
 
-//Post
+ /**
+  * @swagger
+  * /clients/{clientid}/jobs/{jobid}/facilities/{facilityid}/specs/{specid}/charts:
+  *   post:
+  *     summary: Use to create one or more charts
+  *     tags: [Charts]
+  *     requestBody:
+  *       required: true
+  *       content:
+  *         application/json:
+  *           schema:
+  *             type: array
+  *             items:
+  *               $ref: '#/components/schemas/CreateChartBody'
+  *     responses:
+  *       200:
+  *         description: List of created charts
+  *         content:
+  *           application/json:
+  *             schema:
+  *               type: array
+  *               items:
+  *                 $ref: '#/components/schemas/Charts'
+  */
 router.post("/", checkReach, authLvl, dboperations.create_chart);
 
-//DeleteOne
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/facilities/{facilityid}/specs/{specid}/charts/{chartid}:
+ *  delete:
+ *      summary: Use to delete chart by id
+ *      tags: [Charts]
+ *      description: Delete a chart entry
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID of data to delete
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to delete
+ *        - in: path
+ *          name: facilityid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: FacilityID of data to delete
+ *        - in: path
+ *          name: specid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: SpecID of data to delete
+ *        - in: path
+ *          name: chartid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ChartID of data to delete
+ *      responses:
+ *          202:
+ *              description: Deleted chart
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Charts'
+ *          404:
+ *              description: Chart record was not found
+ */
 router.delete("/:chartid", checkReach, authLvl, dboperations.delete_chart);
 
 module.exports = router;
---
+++ schemas/charts.js
@@ -0,0 +1,77 @@
+const yup = require('yup')
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Charts:
+ *       type: object
+ *       properties:
+ *         ID:
+ *           type: int
+ *           description: The public id of the chart
+ *         SpecID:
+ *           type: int
+ *           description: The public id of the client's job facility spec
+ *         SourceField:
+ *           type: string
+ *           description: The source field for the chart data
+ *         ConsumptionText:
+ *           type: int
+ *           description: The consumption text to be displayed on the chart
+ *         UsageField:
+ *           type: int
+ *           description: The field of the file that contains current usage
+ *       example:
+ *         ID: 9999
+ *         SpecID: 101
+ *         SourceField: chart_source
+ *         ConsumptionText: consumption_descriptor
+ *         UsageField: current_usage
+ *     CreateChartBody:
+ *       type: object
+ *       properties:
+ *         SpecID:
+ *           type: int
+ *           description: The public id of the client's job facility spec
+ *         SourceField:
+ *           type: string
+ *           description: The source field for the chart data
+ *         ConsumtpionText:
+ *           type: string
+ *           description: The consumption text to be displayed on the chart
+ *         UsageField:
+ *           type: string
+ *           description: The field of the file that contains current usage
+ *       example:
+ *         SpecID: 101
+ *         SourceField: chart_source 
+ *         ConsumptionText: consumption_descriptor
+ *         UsageField: current_usage
+ *     DeleteChartResponse:
+ *       type: object
+ *       properties:
+ *         ID:
+ *           type: int
+ *           description: The public id of the chart
+ *         SpecID:
+ *           type: int
+ *           description: The public id of the client's job facility spec
+ *       example:
+ *         ID: 9999
+ *         SpecID: 101   
+ */
+
+module.exports = yup
+  .object()
+  .required()
+  .shape({
+    Charts: yup.array().of(
+      yup.object().shape({
+        SpecID: yup.number().integer().required('SpecID is required.'),
+        SourceField: yup.string().required('SourceField for the chart is required'),
+        ConsumptionText: yup.string().required('Consumption Text to be displayed with the chart is required.'),
+        UsageField: yup.string().required('Usage field for the chart is required'),
+      })
+    ),
+  })
