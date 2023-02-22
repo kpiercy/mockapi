53e3088467fbed97e2a2aa07ecaa49fb012af35e
Kraig Piercy <kapiercy11@gmail.com>
2023-02-22 15:27:32 -0500

    create swagger docs for /orders

--- routes/orders.js
+++ routes/orders.js
@@ -22,18 +22,190 @@ router.use(pubip().getIpInfoMiddleware)
 router.use('/:orderid/versions', versionRoutes)
 
 //get all orders for this job by client
-router.get('/', checkReach, dboperations.all_orders)
+//router.get('/', checkReach, dboperations.all_orders)
 
-//get single order for this job by client
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/orders/{orderid}:
+ *  get:
+ *      summary: Get order by id
+ *      tags: [Orders]
+ *      description: Finds order by id
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID or ParentID of data to find
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to find
+ *        - in: path
+ *          name: orderid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: OrderID of data to find
+ *      responses:
+ *          200:
+ *              description: Found order
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            Orders:
+ *                              type: array
+ *                              items:
+ *                                $ref: '#/components/schemas/Order'
+ *          404:
+ *              description: Order record was not found
+ */
 router.get('/:orderid', checkReach, dboperations.one_order)
 
-//update single contact for this job by id
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/orders/{orderid}:
+ *  patch:
+ *      summary: Update order by id
+ *      tags: [Orders]
+ *      description: Update single order by id
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID of data to find
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to find
+ *        - in: path
+ *          name: orderid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: OrderID of data to find
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                Orders:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/UpdateOrdersBody'
+ *      responses:
+ *          200:
+ *              description: Updated order
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            Orders:
+ *                              type: array
+ *                              items:
+ *                                $ref: '#/components/schemas/Order'
+ */
 router.patch('/:orderid', checkReach, dboperations.update_order)
 
-//create new order for this job by client
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/orders:
+ *  post:
+ *      summary: Create orders record for job
+ *      tags: [Orders]
+ *      description: Create order automation record for job
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID of data to find
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to find
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                Orders:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/CreateOrdersBody'
+ *      responses:
+ *          201:
+ *              description: Created order
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            Orders:
+ *                              type: array
+ *                              items:
+ *                                $ref: '#/components/schemas/Order'
+ */
 router.post('/', checkReach, authLvl, dboperations.create_order)
 
-//delete order for this job by client
+/**
+ * @swagger
+ * /clients/{clientid}/jobs/{jobid}/orders/{orderid}:
+ *  delete:
+ *      summary: Delete order by id
+ *      tags: [Orders]
+ *      description: Delete order by id
+ *      parameters:
+ *        - in: path
+ *          name: clientid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: ClientID or ParentID of data to find
+ *        - in: path
+ *          name: jobid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: JobID of data to find
+ *        - in: path
+ *          name: orderid
+ *          schema: 
+ *              type: int
+ *          required: true
+ *          description: OrderID of data to find
+ *      responses:
+ *          202:
+ *              description: Deleted order
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            Orders:
+ *                              type: array
+ *                              items:
+ *                                $ref: '#/components/schemas/DeleteOrderResponse'
+ *          404:
+ *              description: Order record was not found
+ */
 router.delete('/:orderid', checkReach, authLvl, dboperations.delete_order)
 
 module.exports = router;
---
+++ schemas/orders.js
@@ -0,0 +1,135 @@
+const yup = require('yup')
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Order:
+ *       type: object
+ *       properties:
+ *         ID:
+ *           type: int
+ *           description: The public id of the order
+ *         JobID: 
+ *           type: int
+ *           description: The public id of the job
+ *         OrderTemplate:
+ *           type: string
+ *           description: the order number of the template from ERp that will be use to create new monthly orders
+ *         CurrentOrder:
+ *           type: string
+ *           description: the currently active order number in ERP that will have versions added to it
+ *         TemplateVersionID:
+ *           type: int
+ *           description: the version suffix to copy for new versions
+ *         BillingType:
+ *           type: int
+ *           description: 1 is billed simplex equals mail processing, 2 is billed for additional pages line item
+ *         OrdersAutomated:
+ *           type: boolean
+ *           description: whether the order version creation is automated
+ *         PreviousOrder:
+ *           type: string
+ *           description: the previous order number
+ *         NextOrder:
+ *           type: string
+ *           description: the next order number to use in lieu of creating a new monthly order
+ *         InvoicesAutomated:
+ *           type: boolean
+ *           description: whether or not the PreviousOrder will be processed by BulkOrderClose and invoicing processes
+ *       example:
+ *         ID: 99
+ *         JobID: 101
+ *         OrderTemplate: T1234
+ *         CurrentOrder: "182555"
+ *         TemplateVersionID: 0
+ *         BillingType: 1
+ *         OrdersAutomated: true
+ *         PreviousOrder: 
+ *         NextOrder: 
+ *         InvoicesAutomated: true
+ *     CreateOrdersBody:
+ *       type: object
+ *       properties:
+ *         JobID: 
+ *           type: int
+ *           description: The public id of the job
+ *         OrderTemplate:
+ *           type: string
+ *           description: the order number of the template from ERp that will be use to create new monthly orders
+ *         CurrentOrder:
+ *           type: string
+ *           description: the currently active order number in ERP that will have versions added to it
+ *         TemplateVersionID:
+ *           type: int
+ *           description: the version suffix to copy for new versions
+ *         BillingType:
+ *           type: int
+ *           description: 1 is billed simplex equals mail processing, 2 is billed for additional pages line item
+ *         OrdersAutomated:
+ *           type: boolean
+ *           description: whether the order version creation is automated
+ *         InvoicesAutomated:
+ *           type: boolean
+ *           description: whether or not the PreviousOrder will be processed by BulkOrderClose and invoicing processes
+ *       example:
+ *         JobID: 101
+ *         OrderTemplate: T1234
+ *         CurrentOrder: "182555"
+ *         TemplateVersionID: 0
+ *         BillingType: 1
+ *         OrdersAutomated: true
+ *         InvoicesAutomated: true
+ *     UpdateOrdersBody:
+ *       type: object
+ *       properties:
+ *         OrderTemplate:
+ *           type: string
+ *           description: the order number of the template from ERp that will be use to create new monthly orders
+ *         CurrentOrder:
+ *           type: string
+ *           description: the currently active order number in ERP that will have versions added to it
+ *         TemplateVersionID:
+ *           type: int
+ *           description: the version suffix to copy for new versions
+ *         BillingType:
+ *           type: int
+ *           description: 1 is billed simplex equals mail processing, 2 is billed for additional pages line item
+ *         OrdersAutomated:
+ *           type: boolean
+ *           description: whether the order version creation is automated
+ *         TriggerNewOrder:
+ *           type: boolean
+ *           description: trigger creation of new monthly order
+ *         PreviousOrder:
+ *           type: string
+ *           description: previous order number populated after TriggerNewOrder is ran
+ *         NextOrder:
+ *           type: string
+ *           description: the next order number to use in lieu of creating a new monthly order
+ *         InvoicesAutomated:
+ *           type: boolean
+ *           description: whether or not the PreviousOrder will be processed by BulkOrderClose and invoicing processes
+ *       example:
+ *         OrderTemplate: T1234
+ *         CurrentOrder: "182555"
+ *         TemplateVersionID: 0
+ *         BillingType: 1
+ *         OrdersAutomated: true
+ *         TriggerNewOrder: false
+ *         PreviousOrder: 
+ *         NextOrder: 
+ *         InvoicesAutomated: true
+ *     DeleteOrderResponse:
+ *       type: object
+ *       properties:
+ *         ID:
+ *           type: int
+ *           description: The public id of the order
+ *         Active:
+ *           type: boolean
+ *           description: Whether the order is active or not
+ *       example:
+ *         ID: 9999
+ *         Active: false 
+ */
