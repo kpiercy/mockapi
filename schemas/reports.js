3e1c141cddecff16b2d3e0bd1ef668f357b78844
Kraig Piercy <kapiercy11@gmail.com>
2023-02-23 11:55:40 -0500

    add swagger schema for /reports

--- schemas/reports.js
+++ schemas/reports.js
@@ -1,5 +1,111 @@
 const yup = require('yup')
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Report:
+ *       type: object
+ *       properties:
+ *         ID:
+ *           type: int
+ *           description: public id of reporting requirements
+ *         SpecID:
+ *           type: int
+ *           description: public id of facility spec
+ *         SummaryReport:
+ *           type: boolean
+ *           description: Whether facility gets SummaryReports sent
+ *         MovesReport:
+ *           type: boolean
+ *           description: Whether facility gets MovesReports sent
+ *         SuppressionReport:
+ *           type: boolean
+ *           description: Whether facility gets SuppressionReports sent
+ *         UndeliverablesReport:
+ *           type: boolean
+ *           description: Whether facility gets UndeliverablesReports sent
+ *         NonCassReport:
+ *           type: boolean
+ *           description: Whether facility gets NonCassReports sent
+ *         FacilityReport:
+ *           type: boolean
+ *           description: Whether facility gets FacilityReports sent
+ *         FacilityPDFs:
+ *           type: boolean
+ *           description: Whether facility gets FacilityPDFs sent
+ *         PrintPDFs:
+ *           type: boolean
+ *           description: Whether facility gets PrintPDFs sent
+ *         AnomalyReport:
+ *           type: boolean
+ *           description: Whether facility gets AnomalyReports sent
+ *         MonthlySummaryReport:
+ *           type: boolean
+ *           description: Whether facility gets MonthlySummaryReports sent
+ *       example:
+ *         ID: 1
+ *         SpecID: 55
+ *         SummaryReport: true
+ *         MovesReport: true
+ *         Suppressionreport: true
+ *         UndeliverablesReport: true
+ *         NonCassReport: true
+ *         FacilityReport: false
+ *         FacilityPDFs: false
+ *         PrintPDFs: false
+ *         AnomalyReport: true
+ *         MonthlySummaryReport: false
+ *     CreateReportsBody:
+ *       type: object
+ *       properties:
+ *         SpecID:
+ *           type: int
+ *           description: public id of facility spec
+ *         SummaryReport:
+ *           type: boolean
+ *           description: Whether facility gets SummaryReports sent
+ *         MovesReport:
+ *           type: boolean
+ *           description: Whether facility gets MovesReports sent
+ *         SuppressionReport:
+ *           type: boolean
+ *           description: Whether facility gets SuppressionReports sent
+ *         UndeliverablesReport:
+ *           type: boolean
+ *           description: Whether facility gets UndeliverablesReports sent
+ *         NonCassReport:
+ *           type: boolean
+ *           description: Whether facility gets NonCassReports sent
+ *         FacilityReport:
+ *           type: boolean
+ *           description: Whether facility gets FacilityReports sent
+ *         FacilityPDFs:
+ *           type: boolean
+ *           description: Whether facility gets FacilityPDFs sent
+ *         PrintPDFs:
+ *           type: boolean
+ *           description: Whether facility gets PrintPDFs sent
+ *         AnomalyReport:
+ *           type: boolean
+ *           description: Whether facility gets AnomalyReports sent
+ *         MonthlySummaryReport:
+ *           type: boolean
+ *           description: Whether facility gets MonthlySummaryReports sent
+ *       example:
+ *         SpecID: 55
+ *         SummaryReport: true
+ *         MovesReport: true
+ *         Suppressionreport: true
+ *         UndeliverablesReport: true
+ *         NonCassReport: true
+ *         FacilityReport: false
+ *         FacilityPDFs: false
+ *         PrintPDFs: false
+ *         AnomalyReport: true
+ *         MonthlySummaryReport: false
+ */
+
 module.exports = yup.object().shape({
   Reports: yup.array().of(
     yup.object().shape({
