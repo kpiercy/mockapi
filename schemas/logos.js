42e246eff14561891950a82120465fcb50c4b05a
Kraig Piercy <kapiercy11@gmail.com>
2023-03-01 11:04:16 -0500

    add swagger doc for /logos

---
+++ schemas/logos.js
@@ -0,0 +1,15 @@
+const yup = require('yup')
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Logo:
+ *       type: object
+ *       properties:
+ *         Location:
+ *           type: string
+ *           description: local location where the file can be found
+ *       example:
+ *         Location: C:\clients\P\PFS_zAdena\Logos\
+ */
